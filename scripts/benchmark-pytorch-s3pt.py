fb9cb5e850db435c169450d0af0bf781c9b8c800
arzhanov <arzhanov@amazon.com>
2024-04-16 19:34:09 +0200

    add s3pt and fsspec bench tests

--- scripts/benchmark-pytorch-s3pt.py
+++ scripts/benchmark-pytorch-s3pt.py
@@ -9,10 +9,13 @@ import time
 import random
 
 import numpy as np
-import webdataset as wds
 import torch
 from torchvision.transforms import v2 as tvt
 from PIL import Image
+
+import torchdata
+import webdataset as wds
+import s3torchconnector as s3pt
 
 CHANNEL_NAME = 'train'
 DEBUG = True
@@ -113,65 +116,151 @@ def parse_and_validate_args():
     
     return args
 
+def _make_pt_dataset(config, transform):
+    files = glob.glob(config.input_channel + '/**/*.jpg')
+    dataset = MapDataset(files, transform)
+    return dataset
+
+def _make_wds_dataset(config, transform):
+    files = glob.glob(config.input_channel + '/*.tar')
+    dataset = wds.WebDataset(files, shardshuffle=True)
+    dataset = dataset.decode('pil')
+    dataset = dataset.to_tuple('jpg', 'cls')
+    dataset = dataset.map_tuple(transform, identity)
+    return dataset
+
+def _make_s3pt_map_dataset(config, transform):
+
+    def _read_and_transform(sample):
+        key, data = sample.key, sample
+        img = transform(Image.open(data).convert('RGB'))
+        label = int(key.split('/')[-2])
+        return img, label
+
+    dataset = s3pt.S3MapDataset.from_prefix(
+        config.dataset_s3_uri,
+        region=config.dataset_s3_region,
+        transform=_read_and_transform)
+    return dataset
+
+def _make_s3pt_iter_dataset(config, transform):
+
+    def _tar_to_tuple(s3object):
+        return s3object.key, torchdata.datapipes.utils.StreamWrapper(s3object)
+    
+    def _read_and_transform(item, cls_transform=int, jpg_transform=transform):
+        key, value = item
+        if key.endswith('.cls'):
+            return key, cls_transform(value.read())
+        if key.endswith('.jpg'):
+            return key, jpg_transform(Image.open(value).convert('RGB'))
+    
+    def _wds_to_tuple(item):
+        return item['.jpg'], item['.cls']
+
+    dataset = s3pt.S3IterableDataset.from_prefix(
+        config.dataset_s3_uri,
+        region=config.dataset_s3_region)
+    dataset = torchdata.datapipes.iter.IterableWrapper(dataset)
+    if config.num_workers > 0:
+        dataset = dataset.sharding_filter()
+    dataset = dataset.map(_tar_to_tuple)
+    dataset = dataset.load_from_tar() 
+    dataset = dataset.map(_read_and_transform)
+    dataset = dataset.webdataset()
+    dataset = dataset.map(_wds_to_tuple)
+    return dataset
+
+def _make_fsspec_map_dataset(config, transform):
+    
+    def _read_and_transform(sample):
+        key, data = sample
+        img = transform(Image.open(data).convert('RGB'))
+        label = int(key.split('/')[-2])
+        return img, label
+    
+    s3_prefixes = list(torchdata.datapipes.iter.FSSpecFileLister(config.dataset_s3_uri))  # Trick to get list of "subdirs"
+    dataset = torchdata.datapipes.iter.FSSpecFileLister(s3_prefixes)
+    if config.num_workers > 0:
+        dataset = dataset.sharding_filter()
+    dataset = dataset.open_files_by_fsspec(mode='rb')
+    dataset = dataset.map(_read_and_transform)
+    return dataset
+
+
+def _make_fsspec_iter_dataset(config, transform):
+
+    def _read_and_transform(item, cls_transform=int, jpg_transform=transform):
+        key, value = item
+        if key.endswith('.cls'):
+            return key, cls_transform(value.read())
+        if key.endswith('.jpg'):
+            return key, jpg_transform(Image.open(value).convert('RGB'))
+        
+    def _wds_to_tuple(item):
+        return item['.jpg'], item['.cls']
+    
+    dataset = torchdata.datapipes.iter.FSSpecFileLister(config.dataset_s3_uri)
+    if config.num_workers > 0:
+        dataset = dataset.sharding_filter()
+    dataset = dataset.open_files_by_fsspec(mode='rb')
+    dataset = dataset.load_from_tar() 
+    dataset = dataset.map(_read_and_transform)
+    dataset = dataset.webdataset()
+    dataset = dataset.map(_wds_to_tuple)
+    return dataset
+
 
 def build_dataloader(config):
 
     transform = tvt.Compose([
         tvt.ToImage(),
         tvt.ToDtype(torch.uint8, scale=True),
-        tvt.RandomResizedCrop(size=(config.input_dim, config.input_dim), antialias=False),
+        tvt.RandomResizedCrop(size=(config.input_dim, config.input_dim), antialias=False), #antialias=True
         tvt.ToDtype(torch.float32, scale=True),
- #       tvt.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        #tvt.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
     # Build dataset
     if config.input_mode in {'file', 'ffm'}:
         if config.dataset_format == 'jpg':
-            files = glob.glob(config.input_channel + '/**/*.jpg')
-            dataset = MapDataset(files, transform)
-            dataloader_batch_size = config.batch_size
+            dataset = _make_pt_dataset(config, transform)
         elif config.dataset_format == 'tar/jpg':
-            files = glob.glob(config.input_channel + '/*.tar')
-            dataset = (
-                wds.WebDataset(files, shardshuffle=True)
-                .decode('pil')
-                .to_tuple('jpg', 'cls')
-                .map_tuple(transform, identity)
-                .batched(config.batch_size))    # Do we actually need to batch here?
-            dataloader_batch_size = None        # <---------------------|
+            dataset = _make_wds_dataset(config, transform)
         else:
             raise NotImplementedError("Unknown dataset format '%s'.." % config.dataset_format)
     elif config.input_mode == 's3pt':
         if config.dataset_format == 'jpg':
-            dataset = S3MapDataset.from_prefix(config.dataset_s3_uri, region=config.dataset_s3_region, transform=load_sample)
+            dataset = _make_s3pt_map_dataset(config, transform)
         elif config.dataset_format == 'tar/jpg':
-            # ...
+            dataset = _make_s3pt_iter_dataset(config, transform)
     elif config.input_mode == 'fsspec':
         if config.dataset_format == 'jpg':
-            # ...
+            dataset = _make_fsspec_map_dataset(config, transform)
         elif config.dataset_format == 'tar/jpg':
-            # ...
+            dataset = _make_fsspec_iter_dataset(config, transform)
     elif config.input_mode == 's3wds':
         if config.dataset_format == 'jpg':
             raise NotImplementedError("Input mode '%s' supports on 'tar/wds'.." % config.input_mode)
         elif config.dataset_format == 'tar/jpg':
             # ...
+            pass
     else:
         raise NotImplementedError("Input mode '%s' is not implemented.." % config.input_mode)
 
     
     # Debug
-    debug("Total files: %d" % len(files))
-    debug("First 5 files:")
-    for i, f in enumerate(files):
-        if i > 5: break
-        debug('- %s' % f)
+    #debug("Total files: %d" % len(files))
+    #debug("First 5 files:")
+    #for i, f in enumerate(files):
+    #    if i > 5: break
+    #    debug('- %s' % f)
 
     # Build dataloader
     dataloader = torch.utils.data.DataLoader(
         dataset,
         num_workers=config.num_workers,
-        batch_size=dataloader_batch_size,
+        batch_size=config.batch_size,
         prefetch_factor=config.prefetch_size,
         pin_memory=config.pin_memory
     )
@@ -210,8 +299,10 @@ def _train_model_mock(model, dataloader, cfg):
             # do training step
             batch_size = images.shape[0]
             img_tot += batch_size
+
             if cfg.compute_time > 0:
                 model.compute(cfg.compute_time/1000) # ms --> s
+            print(iteration, '-->', images.shape, labels.shape, labels)
 
         # log metrics
         img_tot_list.append(img_tot)
